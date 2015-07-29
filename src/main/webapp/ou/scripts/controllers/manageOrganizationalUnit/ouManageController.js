3efab63338096e9ac927bc40da5aef86341636e8
Radu Hoaghe <Radu.Hoaghe@Teamnet.ro>
2015-07-29 15:13:35 +0300

    Adaugare eveniment de selectare departament pe ecranul de Manage Organizations.

--- src/main/webapp/ou/scripts/controllers/manageOrganizationalUnit/ouManageController.js
+++ src/main/webapp/ou/scripts/controllers/manageOrganizationalUnit/ouManageController.js
@@ -1,26 +1,40 @@
 /**
  * Created by Radu.Hoaghe on 7/16/2015.
  */
-ouControllers.controller('ouManageController', ['$scope', function ($scope) {
+ouControllers.controller('ouManageController', ['$scope', '$http', function ($scope, $http) {
     $scope.departmentSelection = {};
+    $scope.disableDetails = false;
 
-    $scope.departments = [
-        {
-            name: "Departament 1"
-        },
-        {
-            name: "Departament 2"
-        },
-        {
-            name: "Departament 3"
-        },
-        {
-            name: "Departament 4"
-        }
-    ];
-    $scope.departmentSelection.selected = $scope.departments[0];
+    $http.get('/ou/scripts/controllers/manageOrganizationalUnit/ouManage.json')
+        .success(function(data) {
+            $scope.departments = data.content.organizations;
+        })
+        .error(function(data) {
+
+        });
+
+    //$scope.departmentWasSelected = $scope.departmentSelection.selected != null ||
+    //                                $scope.departmentSelection.selected != undefi
     
     $scope.createOrganization = function () {
-        
     }
+
+    //$scope.myFunction = function (data) {
+    //    $scope.token = data;
+    //}
+    //
+    //$scope.submit = function () {
+    //
+    //    if ($scope.token != null || $scope.token != undefined) {
+    //        $http.post('/upload/sendToken', $scope.token)
+    //            .success(function (response) {
+    //                console.log(response.response);
+    //            })
+    //            .error(function (response) {
+    //                console.log(response.response);
+    //            })
+    //    } else {
+    //        window.alert("Please upload file!");
+    //    }
+    //}
 }]);
