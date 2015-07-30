edc9fe1dee78069f7f371b7e3b43b3bce6aacc15
Cojocaru Florin-Alexandru <alexandru.cojocaru11@gmail.com>
2015-07-30 17:50:54 +0300

    UI for managing functions associated to OUs

--- src/main/webapp/ou/scripts/controllers/organizationalUnit/organizationalUnitController.js
+++ src/main/webapp/ou/scripts/controllers/organizationalUnit/organizationalUnitController.js
@@ -1,6 +1,6 @@
 'use strict';
 ouControllers
-    .controller('organizationalUnitController',['$scope', 'organizationalUnitService', function ($scope, organizationalUnitService) {
+    .controller('organizationalUnitController',['$scope', 'organizationalUnitService', 'organizationalUnitServiceMock', 'AccountMock', 'Function', function ($scope, organizationalUnitService, organizationalUnitServiceMock, AccountMock, Function) {
 
         $scope.showDropDownForPerspective = false;
         $scope.isPerspectiveSelected = false;
@@ -14,6 +14,100 @@ ouControllers
         var baseTemplateUrl = 'ou/views/organizationalUnit/template/';
         $scope.accountsTpl = baseTemplateUrl + 'account.tpl.html';
         $scope.functionsTpl = baseTemplateUrl + 'function.tpl.html';
+
+        $scope.organizationalUnits = [];
+        $scope.selectedOu = {};
+        $scope.accounts = [];
+        $scope.functions = [];
+
+        $scope.search = '';
+        $scope.selectedSearch = '';
+
+        $scope.isView = true;
+        $scope.isEdit = false;
+
+        $scope.loading = false;
+
+        $scope.isSelected = function(organizationalUnit){
+            return organizationalUnit.id === $scope.selectedOu.id;
+        };
+
+        $scope.findByProperty = function(array, key, val) {
+            for (var i = 0; i < array.length; i++) {
+                if (array[i][key] === val) {
+                    return array[i];
+                }
+            }
+            return false;
+        };
+
+        $scope.selectOrganizationalUnit = function(organizationalUnit) {
+            $scope.loading = true;
+            //TODO getByCode in repository & service (backend + frontend)
+            organizationalUnitServiceMock.getByCode({ouCode: organizationalUnit.code}, function(res) {
+
+                $scope.selectedOu = res;
+
+                $scope.functions = angular.copy($scope.allFunctions);
+                $scope.accounts = angular.copy($scope.allAccounts);
+            });
+        };
+
+        $scope.editOrganizationalUnit = function(){
+            $scope.isView = false;
+            $scope.isEdit = true;
+        };
+
+        $scope.saveOrganizationalUnit = function() {
+
+            //TODO updateOrganizationalUnit in repository & service (backend + frontend)
+            organizationalUnitServiceMock.updateOrganizationalUnit($scope.selectedOu, function() {
+                Notification.success('OrganizationalUnit updated');
+                $scope.backOrganizationalUnit();
+            }, function(error) {
+                Notification.error(error.data.errMsg);
+            });
+        };
+
+        $scope.backOrganizationalUnit = function(){
+            $scope.isView = true;
+            $scope.isEdit = false;
+
+            if($scope.selectedOu.id != undefined){
+                //$scope.selectOrganizationalUnit($scope.selectedOu);
+            } else {
+                //$scope.selectOrganizationalUnit($scope.organizationalUnits[0]);
+            }
+        };
+
+        $scope.startFnc = function(){
+            arguments[0].target.style.visibility = 'hidden';
+        };
+
+        $scope.stopFnc = function(){
+            arguments[0].target.style.visibility = '';
+        };
+
+        var init = function() {
+            //TODO getAllOrganizationalUnits in service & repository (backend + frontend)
+            organizationalUnitServiceMock.getAllOrganizationalUnits({}, function(res) {
+                $scope.organizationalUnits = res;
+                AccountMock.getAll(function(res) {
+                    $scope.allAccounts = res;
+                    if(!_.isEmpty($scope.organizationalUnits)) {
+                        $scope.selectOrganizationalUnit($scope.organizationalUnits[0]);
+                    }
+                });
+                Function.getAll(function(res){
+                    $scope.allFunctions = res;
+                    if(!_.isEmpty($scope.organizationalUnits)){
+                        $scope.selectOrganizationalUnit($scope.organizationalUnits[0]);
+                    }
+                });
+            });
+        };
+
+        init();
 
         $scope.showPerspectiveSelect = function () {
             $scope.showDropDownForPerspective = true;
